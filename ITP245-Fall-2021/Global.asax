78db19b6496a9c043aeac91536eb03268aaf5f7c
Bob Dust <bdust@reynolds.edu>
2021-09-08 08:08:24 -0400

    Add project files.

---
+++ ITP245-Fall-2021/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITP245_Fall_2021.MvcApplication" Language="C#" %>
---
+++ ITP245-Fall-2021/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITP245_Fall_2021.Startup))]
+namespace ITP245_Fall_2021
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
